2fdcfd02955491eeb0a559f88248d0bc19528766
Nosheene MOHAMMAD <mohammadnosheene@gmail.com>
2026-02-04 23:56:54 +0100

    P_contact & P_nos_menus

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,10 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/contact", "contact", "/pages/contact.html"),
+    new Route("/nos_menus", "nos menus", "/pages/nos_menus.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Vite & Gourmand";
